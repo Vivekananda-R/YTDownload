81cff938e5b13f055cd15bbb99211729d524457c
Vivekananda-R <vickyrbhat@gmail.com>
2024-01-07 17:55:21 +0530

    add resolution and prefix naming options

--- ytdownload.py
+++ ytdownload.py
@@ -1,4 +1,5 @@
 import typer
+from enum import Enum
 from typing import Annotated,Optional,List
 from pytube import YouTube,Playlist
 import os
@@ -6,7 +7,17 @@ import concurrent.futures
 
 cli=typer.Typer()
 
-def download(url,folder=os.path.join(os.path.expanduser('~'),'Downloads')):
+class ResolutionTypes(str,Enum):
+    low="low"
+    high="high"
+    p720="720p"
+    p480="480p"
+    p360="360p"
+    p240="240p"
+    p144="144p"
+
+def download(url,folder=os.path.join(os.path.expanduser('~'),'Downloads'),resolution:ResolutionTypes=ResolutionTypes.p720
+             ,name=None):
     try:
         yt_object=YouTube(url.strip('"'))
         print(yt_object.title)
@@ -14,18 +25,40 @@ def download(url,folder=os.path.join(os.path.expanduser('~'),'Downloads')):
         return 'ERROR: The link is not recognized as a Youtube Link'
         
     try:
-        video=yt_object.streams.get_highest_resolution()
-        path=video.download(folder)
+        if resolution=="high":
+            video=yt_object.streams.get_highest_resolution()
+        elif resolution=="low":
+            video=yt_object.streams.get_lowest_resolution()
+        else:
+            video=yt_object.streams.get_by_resolution(resolution)
+            
+    except Exception as e:
+        return 'ERROR: Provide a valid resolution'
+    
+    try:
+        
+        if name is None:
+            path=video.download(fr'{folder}')
+        else:
+            path=video.download(fr'{folder}',filename_prefix=name)
+            
         video_name=path.split('\\')[-1].replace('.mp4','')
+        
     except Exception as e:
         return 'ERROR: Could\'nt download the video'
         
     return f'{video_name} is downloaded!!'
 
+
+
 @cli.command()
 def url_downloads(urls: Annotated[List[str],typer.Argument(help="Provide youtube video url, multiple urls separated by spaces")]=None,
                   playlist:Annotated[Optional[List[str]],typer.Option('--playlist','-p',help="Provide a playlist link for downloading whole playlist")]=None,
-                  folder:Annotated[str,typer.Option('--folder','-f',help="Provide a folder path for download")]=os.path.join(os.path.expanduser('~'),'Downloads')):
+                  folder:Annotated[str,typer.Option('--folder','-f',help="Provide a folder path for download")]=os.path.join(os.path.expanduser('~'),'Downloads'),
+                  resolution:Annotated[ResolutionTypes,typer.Option('--resolution','-r',case_sensitive=False,
+                            help="Provide video resolution types from [high,low,p720,p480,p360,p240]")]=ResolutionTypes.p720,
+                  name:Annotated[str,typer.Option('--name','-n',help="Provide a prefix name for single YT video download")]=None):
+    
     if not urls and not playlist:
         if folder!=os.path.join(os.path.expanduser('~'),'Downloads'):
             print("ERROR: Please provide a YouTube link to download")
@@ -48,6 +81,8 @@ All in one place to download any youtube video
             print("ERROR: Folder could not be created!")
             return  
     
+    result=[] 
+      
     if playlist is not None:
         # print(playlist)
         
@@ -56,12 +91,31 @@ All in one place to download any youtube video
                 print(f"NOTE: The link \"{p}\" is a Normal YouTube video link, please remove -p flag and try again")
                 continue
             pl=Playlist(p)
-            print("Playlist:",pl.title)
-            for u in pl.video_urls:
-                urls.append(u)
+              
+            print("Please wait while downloading the Playlist:",pl.title)
+            with concurrent.futures.ThreadPoolExecutor(max_workers=(os.cpu_count())//2) as executor:
+                threads=[]
+                for i,u in enumerate(pl.video_urls):    
+                    if "youtube" not in u:
+                        print(f"ERROR: {u} is not a valid youtube link\n\tProvide a vaild url starting with 'http://www.youtube.com/'")
+                        continue
+                    try:
+                        yt_object=YouTube(u.strip('"'))
+                        print(yt_object.default_filename)
+                    except Exception as e:
+                        print(f"ERROR: The link \"{u}\" for {yt_object.title} is not recognized as a Youtube Link")
+                        continue 
+                    name=fr'{i+1}- '
+                    
+                    u=u.split("&t")[0]
+                
+                    threads.append(executor.submit(download, u,folder,resolution,name))
+                    
+                for future in concurrent.futures.as_completed(threads):
+                    result.append(future.result())
+                
             
                 
-    result=[]   
     if urls is not None:
         # print(urls)
         if len(urls)==1:
@@ -72,10 +126,11 @@ All in one place to download any youtube video
             if "list=PL" in urls[0]:
                 print(f"NOTE: The link is a Playlist, please use -p flag to download a playlist!")
                 return
-            result.append(download(urls[0].split("&t")[0],folder))
+            result.append(download(urls[0].split("&t")[0],folder,resolution,fr'{name} - ' if name is not None else None))
             
         else:    
             print('Please wait while downloading videos:')
+                
             with concurrent.futures.ThreadPoolExecutor(max_workers=(os.cpu_count())//2) as executor:
                     threads=[]
                     for u in urls:
@@ -86,7 +141,7 @@ All in one place to download any youtube video
                             print(f"NOTE: The link is a Playlist, please use -p flag to download a playlist!")
                             continue
                         u=u.split("&t")[0]
-                        threads.append(executor.submit(download, u,folder))
+                        threads.append(executor.submit(download, u,folder,resolution,None))
                         
                     for future in concurrent.futures.as_completed(threads):
                         result.append(future.result())
